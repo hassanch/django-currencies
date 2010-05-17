3fd4c00d9204eed83f323b5134f2d4bfcf7ea448
hassanch <hassan.chahrour@greenmangaming.com>
2010-05-17 13:10:10 +0100

    Changed code to primary_key, save is more thorough, replaced hard tabs with soft tabs.

--- currencies/models.py
+++ currencies/models.py
@@ -3,30 +3,29 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Currency(models.Model):
-	code = models.CharField(_('code'), max_length=3)
-	name = models.CharField(_('name'), max_length=25)
-	symbol = models.CharField(_('symbol'), max_length=1)
-	factor = models.DecimalField(_('factor'), max_digits=10, decimal_places=4,
-		help_text=_('Specifies the difference of the currency to default one.'))
-	is_active = models.BooleanField(_('active'), default=True,
-		help_text=_('The currency will be available.'))
-	is_default = models.BooleanField(_('default'), default=False,
-		help_text=_('Make this the default currency.'))
+    code = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=25)
+    symbol = models.CharField(max_length=1)
+    factor = models.DecimalField(max_digits=10, decimal_places=4,
+        help_text=_('Specifies the difference of the currency to default one.'))
+    is_active = models.BooleanField(default=True,
+        help_text=_('The currency will be available.'))
+    is_default = models.BooleanField(default=False,
+        help_text=_('Make this the default currency.'))
 
-	class Meta:
-		verbose_name = _('currency')
-		verbose_name_plural = _('currencies')
+    class Meta:
+        verbose_name_plural = _('Currencies')
 
-	def __unicode__(self):
-		return self.code
+    def __unicode__(self):
+        return self.code
 
-	def save(self, **kwargs):
-		if self.is_default:
-			try:
-				default_currency = Currency.objects.get(is_default=True)
-			except DoesNotExist:
-				pass
-			else:
-				default_currency.is_default = False
-				default_currency.save()
-		super(Currency, self).save(**kwargs)
+    def save(self, **kwargs):
+        """Sets all is_default currencies to not default if this is default."""
+        if self.is_default:
+            defaults = Currency.objects.filter(is_default=True)
+            if self.pk:
+                defaults = defaults.exclude(pk=self.pk)
+            for currency in defaults:
+                currency.is_default = False
+                currency.save()
+        super(Currency, self).save(**kwargs)
