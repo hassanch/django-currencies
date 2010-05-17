2433cdc156728e05c7a1a4aa2764babb9528e163
hassanch <hassan.chahrour@greenmangaming.com>
2010-05-17 13:11:08 +0100

    Added .gitignore. Replaced hard tabs with soft tabs.

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ from currencies import __version__
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir:
-	os.chdir(root_dir)
+    os.chdir(root_dir)
 
 for dirpath, dirnames, filenames in os.walk('currencies'):
-	# Ignore dirnames that start with '.'
-	for i, dirname in enumerate(dirnames):
-		if dirname.startswith('.'): del dirnames[i]
-	if '__init__.py' in filenames:
-		pkg = dirpath.replace(os.path.sep, '.')
-		if os.path.altsep:
-			pkg = pkg.replace(os.path.altsep, '.')
-		packages.append(pkg)
-	elif filenames:
-		prefix = dirpath[13:] # Strip "currencies/" or "currencies\"
-		for f in filenames:
-			data_files.append(os.path.join(prefix, f))
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[13:] # Strip "currencies/" or "currencies\"
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
 
 rst_desc = '''
 Introduction
@@ -91,21 +91,21 @@ The settings.py is inside the tests/ directory, so you'll need to cd to it, and:
 '''
 
 setup(name='django-currencies',
-	version=__version__,
-	description='Adds support for multiple currencies as a Django application.',
-	long_description=rst_desc,
-	author='Panos Laganakos',
-	author_email='panos.laganakos@gmail.com',
-	url='http://code.google.com/p/django-currencies/',
-	package_dir={'currencies': 'currencies'},
-	packages=packages,
-	package_data={'currencies': data_files},
-	classifiers=['Development Status :: 4 - Beta',
-		'Environment :: Web Environment',
-		'Framework :: Django',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: BSD License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Topic :: Utilities'],
-	)
+    version=__version__,
+    description='Adds support for multiple currencies as a Django application.',
+    long_description=rst_desc,
+    author='Panos Laganakos',
+    author_email='panos.laganakos@gmail.com',
+    url='http://code.google.com/p/django-currencies/',
+    package_dir={'currencies': 'currencies'},
+    packages=packages,
+    package_data={'currencies': data_files},
+    classifiers=['Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'],
+    )
