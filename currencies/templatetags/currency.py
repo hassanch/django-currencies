6787c150d579175e856f2cd8ef4c29b8832b8a73
hassanch <hassan.chahrour@greenmangaming.com>
2010-05-17 13:13:04 +0100

    Replaced hard tabs with soft tabs.

--- currencies/templatetags/currency.py
+++ currencies/templatetags/currency.py
@@ -8,42 +8,42 @@ register = template.Library()
 
 
 def _calculate_price(price, currency):
-	try:
-		factor = Currency.objects.get(code__exact=currency).factor
-	except Currency.DoesNotExist:
-		if settings.DEBUG:
-			raise Currency.DoesNotExist
-		else:
-			factor = Decimal('0.0')
-	new_price = Decimal(price) * factor
-	return new_price.quantize(Decimal('.01'), rounding=ROUND_UP)
+    try:
+        factor = Currency.objects.get(code__exact=currency).factor
+    except Currency.DoesNotExist:
+        if settings.DEBUG:
+            raise Currency.DoesNotExist
+        else:
+            factor = Decimal('0.0')
+    new_price = Decimal(price) * factor
+    return new_price.quantize(Decimal('.01'), rounding=ROUND_UP)
 
 
 @register.filter(name='currency')
 @stringfilter
 def set_currency(value, arg):
-	return _calculate_price(value, arg)
+    return _calculate_price(value, arg)
 
 
 class ChangeCurrencyNode(template.Node):
 
-	def __init__(self, price, currency):
-		self.price = template.Variable(price)
-		self.currency = template.Variable(currency)
+    def __init__(self, price, currency):
+        self.price = template.Variable(price)
+        self.currency = template.Variable(currency)
 
-	def render(self, context):
-		try:
-			return _calculate_price(self.price.resolve(context),
-				self.currency.resolve(context))
-		except template.VariableDoesNotExist:
-			return ''
+    def render(self, context):
+        try:
+            return _calculate_price(self.price.resolve(context),
+                self.currency.resolve(context))
+        except template.VariableDoesNotExist:
+            return ''
 
 
 @register.tag(name='change_currency')
 def change_currency(parser, token):
-	try:
-		tag_name, current_price, new_currency = token.split_contents()
-	except ValueError:
-		raise template.TemplateSyntaxError, \
-			'%r tag requires exactly two arguments' % token.contents.split()[0]
-	return ChangeCurrencyNode(current_price, new_currency)
+    try:
+        tag_name, current_price, new_currency = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, \
+            '%r tag requires exactly two arguments' % token.contents.split()[0]
+    return ChangeCurrencyNode(current_price, new_currency)
